4223ee660d45d06247a2b9f3634feca097e577ae
jihao <jacky.jihao@gmail.com>
2012-06-02 13:40:11 +0800

    be helpful with build in support (OptionParser and GLI).

--- db_backup/bin/db_backup.rb
+++ db_backup/bin/db_backup.rb
@@ -5,14 +5,19 @@ require 'optparse'
 
 options = {}
 option_parser = OptionParser.new do |opts| 
-  opts.on("-i","--iteration") do
+  executable_name = File.basename $PROGRAM_NAME
+  opts.banner = "  Backup one or more MySQL databases
+  Usgae: #{executable_name} [options] database_name
+  "
+  
+  opts.on("-i","--iteration",'Indicate that this back up is an "iteration" backup') do
     options[:iteration] = true
   end
   #使用正则表达式校验参数
-  opts.on("-u USER", /^(.+)\.(.+)$/) do |user|
+  opts.on("-u USER", "--user USER",/^(.+)\.(.+)$/,'Database username in first.last format') do |user|
     options[:user] = user
   end
-  opts.on("-p PASSWORD") do |password|
+  opts.on("-p PASSWORD",'Database password') do |password|
     options[:password] = password
   end
   opts.on("-t","--[no-]test") do |val|
@@ -29,4 +34,13 @@ option_parser = OptionParser.new do |opts|
 end
 
 option_parser.parse!
+if ARGV.empty?
+  puts "error: you must supply a database_name"
+  puts
+  puts option_parser.help
+  exit 1
+else
+  database_name = ARGV[0]
+end
+
 puts options.inspect
