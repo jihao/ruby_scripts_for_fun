31416aa59a932e9fcf7a2caeaf120f04aa873a16
Hao Ji <jacky.jihao@gmail.com>
2012-06-27 17:02:03 +0800

    very first attemp to update the login message of linux

---
+++ cloud_machine_message/README.md
@@ -0,0 +1,6 @@
+# Introduction
+ Aims to implement a tool to manage all our target machines usage.
+ If someone claims to use a machine, he can update the web with message and duration.
+ This tool automatically changes login message of the machine.
+
+
---
+++ cloud_machine_message/gateway.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby -wKU
+require "net/ssh/gateway"
+
+msg = "welcome ^_^"
+
+gateway = Net::SSH::Gateway.new('150.236.223.68','ema',{:password=>'ema00000'})
+
+gateway.ssh("10.170.67.84", "root", {:password=>"root000"}) do |ssh|
+    puts ssh.exec!("hostname")
+    puts ssh.exec!("echo #{msg} > /etc/motd")
+    puts ssh.exec!("cat /etc/motd") # => print welcome
+end
+gateway.ssh("10.170.67.84", "root", {:password=>"root000"}) do |ssh|
+    puts ssh.exec!("hostname")
+    puts ssh.exec!("cat /etc/motd") # => empty, why?
+end
+gateway.shutdown!
